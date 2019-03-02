85889f131c435182f4dd75e9ceb2a36717492f63
Andrew Jones <andrewjones@Andrews-MacBook-Pro.local>
2019-03-02 19:12:17 +1000

    Remove Digit stuff

--- R/Utility_functions.R
+++ R/Utility_functions.R
@@ -8,19 +8,23 @@
 #'@name SSOSVM
 NULL
 
-#'@importFrom grDevices gray
-#'@importFrom graphics image
-NULL
-
-#'TITLE
-#'@description THIS FUNCTION DOES
-#'@param PARAM1
-#'@param PARAM2
-#'@return RETURNVAL
+#'Generate Simulations
+#'@description Generate simple simulations for testing of the algorithms.
+#'@param NN Number of observations. Default is 10^4
+#'@param DELTA Separation of thre groups in standard errors. Default is 2.
+#'@param DIM Number of dimensions in data. Default is 2.
+#'@param seed Random seed if desired.
+#'@return A list containing:
+#'\item{XX}{Coordinates of the simulated points.}
+#'\item{YY}{Cluster membership of the simulated points.}
+#'#'\item{YMAT}{YY and XX Combined as a single matrix.}
 #'@examples
+#'#100 points of dimension 4.
+#'generateSim(NN=100, DELTA=2, DIM=4)
 #'@export
 generateSim <- function(NN = 10^4, DELTA = 2, DIM = 2, seed=NULL) {
   if(!is.null(seed)){set.seed(seed)}
+  
   # Simulation
   XX <- mvtnorm::rmvnorm(round(NN/2),rep(DELTA,DIM))
   XX <- rbind(XX,mvtnorm::rmvnorm(NN-round(NN/2),rep(-DELTA,DIM)))
@@ -31,57 +35,5 @@ generateSim <- function(NN = 10^4, DELTA = 2, DIM = 2, seed=NULL) {
   YY <- YY[SAMPLE]
   YMAT <- cbind(YY,YY*XX)
   
-  return(list(YMAT=YMAT, XX=XX, YY=YY))
+  return(list(XX=XX, YY=YY,YMAT=YMAT))
 }
-
-
-# helper function for visualization
-#'TITLE
-#'@description THIS FUNCTION DOES
-#'@param PARAM1
-#'@param PARAM2
-#'@return RETURNVAL
-#'@examples
-#'@export
-show_digit = function(arr784, col = gray(12:1 / 12), ...) {
-  image(matrix(as.matrix(arr784[-785]), nrow = 28)[, 28:1], col = col, ...)
-}
-
-# load image files
-#'TITLE
-#'@description THIS FUNCTION DOES
-#'@param PARAM1
-#'@param PARAM2
-#'@return RETURNVAL
-#'@examples
-#'@export
-load_image_file = function(filename) {
-  ret = list()
-  f = file(filename, 'rb')
-  readBin(f, 'integer', n = 1, size = 4, endian = 'big')
-  n    = readBin(f, 'integer', n = 1, size = 4, endian = 'big')
-  nrow = readBin(f, 'integer', n = 1, size = 4, endian = 'big')
-  ncol = readBin(f, 'integer', n = 1, size = 4, endian = 'big')
-  x = readBin(f, 'integer', n = n * nrow * ncol, size = 1, signed = FALSE)
-  close(f)
-  data.frame(matrix(x, ncol = nrow * ncol, byrow = TRUE))
-}
-
-# load label files
-#'TITLE
-#'@description THIS FUNCTION DOES
-#'@param PARAM1
-#'@param PARAM2
-#'@return RETURNVAL
-#'@examples
-#'@export
-load_label_file = function(filename) {
-  f = file(filename, 'rb')
-  readBin(f, 'integer', n = 1, size = 4, endian = 'big')
-  n = readBin(f, 'integer', n = 1, size = 4, endian = 'big')
-  y = readBin(f, 'integer', n = n, size = 1, signed = FALSE)
-  close(f)
-  y
-}
-
-
